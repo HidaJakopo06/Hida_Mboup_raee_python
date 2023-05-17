ff5e9f1615c21669388b103f402d495973065ab5
HidaJakopo06 <hida.jakopo.06@itisdalmine.edu.it>
2023-05-17 09:56:59 +0200

    Commit,codice terminato

--- RAEE.py
+++ RAEE.py
@@ -1,66 +1,80 @@
-import os
-
+import os,time
 
 def clear():
     os.system("cls")
 
 
 def menu():
+
     print("""
-        - Grandi elettrodomestici;
-        - Piccoli elettrodomestici;
-        - Apparecchiature informatiche e per telecomunicazioni;
-        - Apparecchiature di consumo e pannelli fotovoltaici;
-        - Apparecchiature di illuminazione;
-        - Utensili elettrici ed elettronici;
-        - Giocattoli e apparecchiature per il tempo libero e lo sport;
-        - Dispositivi medici (ad eccezione di tutti i prodotti impiantati ed infettati;
-        - Strumenti di monitoraggio e di controllo;
-        - Distributori automatici.
+        -Frigoriferi;
+        -Condizionatori;
+        -Congelatori;
+        -Lavastoviglie;
+        -Lavatrici;
+        -Tv;
+        -Elettroutensili;
+        -Giocattoli;
+        -Apparecchi di illuminazione;
+        -Dispositivi medici
+        -Piccoli elettrodomestici
+        -Sorgenti luminose compatte
+        -Lampade fluorescenti
         """)
 
 
-def smaltimentoLista():
-
-    clear()
-    n = int(input("""
-        1. Grandi elettrodomestici;
-        2. Piccoli elettrodomestici;
-        3. Apparecchiature informatiche e per telecomunicazioni;
-        4. Apparecchiature di consumo e pannelli fotovoltaici;
-        5. Apparecchiature di illuminazione;
-        6. Utensili elettrici ed elettronici;
-        7. Giocattoli e apparecchiature per il tempo libero e lo sport;
-        8. Dispositivi medici (ad eccezione di tutti i prodotti impiantati ed infettati;
-        9. Strumenti di monitoraggio e di controllo;
-        10. Distributori automatici.
-    
-    """))
-
 
 def leggiRifiuto():
-    clear()
-    menu()
-    rifiuto = input("Inserisci il rifiuto : ")
-    return rifiuto
+    while True:
+        raee1 = ["Frigoriferi", "Condizionatori", "Congelatori"]
+        raee2 = ["Lavastoviglie", "Lavatrici"]
+        raee3 = ["Tv", "Monitor"]
+        raee4 = ["Elettroutensili", "Giocattoli", "Apparrecchi di illuminazione",
+                "Dispositivi medici", "Piccoli elettrodomestici"]
+        raee5 = ["Sorgenti luminose compatte", " Lampade fluorescenti"]
+        raee = [raee1, raee2, raee3, raee4, raee5]
+
+        clear()
+        menu()
+        rifiuto = input("Inserisci il rifiuto : ")
+        for lista in raee:
+            for elemento in lista:
+                if elemento == rifiuto:
+                    return rifiuto
+            
 
 
 def sceltaRaee():
+    while True:
+        raee1 = ["Frigoriferi", "Condizionatori", "Congelatori"]
+        raee2 = ["Lavastoviglie", "Lavatrici"]
+        raee3 = ["Tv", "Monitor"]
+        raee4 = ["Elettroutensili", "Giocattoli", "Apparrecchi di illuminazione",
+                "Dispositivi medici", "Piccoli elettrodomestici"]
+        raee5 = ["Sorgenti luminose compatte", "Lampade fluorescenti"]
+        raee = [raee1, raee2, raee3, raee4, raee5]
+        rifiuto = leggiRifiuto()
+        clear()
+        r = 0
+        for lista in raee:
+            r += 1 
+            for elemento in lista:
+                if elemento == rifiuto:
+                    print("Raee Trovato...")
+                    r = str(r)
+                    return "R"+r  
 
-    raee1 = ["Frigoriferi", "Condizionatori", "Congelatori"]
-    raee2 = ["Lavastoviglie", "Lavatrici"]
-    raee3 = ["Tv", "Monitor"]
-    raee4 = ["Elettrotensili", "Giocattoli", "Apparrecchi di illuminazione",
-             "Dispositivi medici", "Piccoli elettrodomestici"]
-    raee5 = ["Sorgenti luminose compatte", " Lampade fluorescenti"]
-    raee = [raee1, raee2, raee3, raee4, raee5]
-    rifiuto = leggiRifiuto()
+
+yes = True
+
+while yes:
+    
+    r = sceltaRaee()
+    print("Il Raee del rifiuto è :",r)
+    time.sleep(2.5)
+    yes = input("Y per continuare - N per terminare: ")
+    if yes == "N":
+        yes = False
     clear()
 
-    for lista in raee:
-        for elemento in lista:
-            if elemento == rifiuto:
-                print("GYYAAAAT")
-
-rifiuto = smaltimentoLista()
-sceltaRaee()
+    
