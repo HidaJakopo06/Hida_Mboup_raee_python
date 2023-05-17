70775e003ac86f3051f9c67a6d1526ce62985f9c
HidaJakopo06 <hida.jakopo.06@itisdalmine.edu.it>
2023-05-17 08:57:52 +0200

    Nuove_liste

--- RAEE.py
+++ RAEE.py
@@ -1,4 +1,30 @@
-import os
+#5 categorie di rifiuti 
+
+def menù_smaltimento():
+    print("""
+        
+            Benvenuti nel menù di smaltimento del gruppo HIDA_ASSANE:
+    
+    
+    """)
+
+def liste_livelli():
+
+    pass
 
 
-os.system("git commit")
+
+
+raee1 = [ "Frigoriferi" , "Condizionatori", "Congelatori" ]
+
+
+raee2 = [ "Lavastoviglie" , "Lavatrici" ]
+
+
+raee3 = ["Tv","Monitor"]
+
+
+raee4 = ["Elettrotensili","Giocattoli","Apparrecchi di illuminazione","Dispositivi medici","Piccoli elettrodomestici"]
+
+
+raee5 = ["Sorgenti luminose compatte"," Lampade fluorescenti"]
